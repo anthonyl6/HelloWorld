e8446f53336c77e977914ffdd3c8abc79d0552df
anthonyl6 <anthonyl6@learn.cssd.ab.ca>
2017-11-30 14:26:52 -0700

    added hello

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hello");
         }
     }
 }
